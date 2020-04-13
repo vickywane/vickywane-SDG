6c0f77d6bdc647e65904b9683767f836cdb1562e
Nwani Victory <vickywane@gmail.com>
2020-04-13 02:38:01 +0100

    ....

--- src/server.js
+++ src/server.js
@@ -1,89 +1,140 @@
-/* eslint-disable comma-dangle */
-/* eslint-disable no-console */
 import express from 'express';
-import debug from 'debug';
-import response from 'response-time';
+import bodyParser from 'body-parser';
+import xml from 'xml';
+import morgan from 'morgan';
 import fs from 'fs';
-import convert from 'xml-js';
-import covid19ImpactEstimator from './estimator';
+import path from 'path';
+
+import estimator from './estimator';
 
 const app = express();
-const PORT = 3000;
+app.use(bodyParser.json());
 
-// TODO: make sure log file doesnt get overwritten and pull data from req body & add status code
+fs.mkdirSync(path.join(__dirname, './logs/'));
+const logFile = fs.createWriteStream(path.join(__dirname, './log.txt'), {
+  flags: 'a',
+});
 
 app.use(
-  response((req, res, time) => {
-    const reqTime = time * 60;
-    const reqMethod = req.method;
-    const path = req.originalUrl;
-    console.log(path);
-
-    const log = {
-      req: reqMethod,
-      reqpath: path,
-      time: reqTime
-    };
-
-    const parsedData = JSON.stringify(log);
-    const write = fs.createWriteStream('./src/logs.json', { flag: 'a' }, (err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log('written log data');
-        write.write(parsedData);
-      }
-    });
-  })
+  morgan(
+    (tokens, req, res) => {
+      return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        parseInt(tokens['response-time'](req, res).toString()) < 10
+          ? '0' + parseInt(tokens['response-time'](req, res).toString()) + 'ms'
+          : parseInt(tokens['response-time'](req, res).toString()) + 'ms',
+      ].join('\t\t');
+    },
+    {
+      stream: logFile,
+    }
+  )
 );
 
-app.post('/api/v1/on-covid-19(*)', (req, res) => {
-  const path = req.originalUrl;
-  // const EstimatorData = req.query.data;
+app.post('/api/v1/on-covid-19', estimator, (req, res) => {
+  res.status(201).json(res.body);
+});
 
-  const test = {
-    region: {
-      name: 'Africa',
-      avgAge: 19.7,
-      avgDailyIncomeInUSD: 5,
-      avgDailyIncomePopulation: 0.71,
-    },
-    periodType: 'days',
-    timeToElapse: 58,
-    reportedCases: 674,
-    population: 66622705,
-    totalHospitalBeds: 1380614,
-  };
-  const data = covid19ImpactEstimator(test);
+app.post('/api/v1/on-covid-19/:type', estimator, (req, res) => {
+  if (req.params.type === 'json') {
+    res.status(201).json(res.body);
+  } else if (req.params.type === 'xml') {
+    const { data, impact, severeImpact } = res.body;
 
-  try {
-    const converted = convert.js2xml(data, { compact: true });
-    if (/x/.test(path)) {
-      res.send(converted);
-    } else {
-      res.send(converted);
-    }
-  } catch (error) {
-    console.log('err occured with passed data', error);
+    const dataToSubmit = [
+      {
+        results: [
+          { _attr: { from: 'COVID-19 estimator' } },
+          {
+            data: [
+              {
+                region: [
+                  { name: data.region.name },
+                  { avgAge: data.region.avgAge },
+                  { avgDailyIncomeInUSD: data.region.avgDailyIncomeInUSD },
+                  {
+                    avgDailyIncomePopulation:
+                      data.region.avgDailyIncomePopulation,
+                  },
+                ],
+              },
+              { periodType: data.periodType },
+              { timeToElapse: data.timeToElapse },
+              { reportedCases: data.reportedCases },
+              { population: data.population },
+              { totalHospitalBeds: data.totalHospitalBeds },
+            ],
+          },
+          {
+            impact: [
+              { currentlyInfected: impact.currentlyInfected },
+              { infectionsByRequestedTime: impact.infectionsByRequestedTime },
+              { severeCasesByRequestedTime: impact.severeCasesByRequestedTime },
+              {
+                hospitalBedsByRequestedTime: impact.hospitalBedsByRequestedTime,
+              },
+              { casesForICUByRequestedTime: impact.casesForICUByRequestedTime },
+              {
+                casesForVentilatorsByRequestedTime:
+                  impact.casesForVentilatorsByRequestedTime,
+              },
+              { dollarsInFlight: impact.dollarsInFlight },
+            ],
+          },
+          {
+            severeImpact: [
+              { currentlyInfected: severeImpact.currentlyInfected },
+              {
+                infectionsByRequestedTime:
+                  severeImpact.infectionsByRequestedTime,
+              },
+              {
+                severeCasesByRequestedTime:
+                  severeImpact.severeCasesByRequestedTime,
+              },
+              {
+                hospitalBedsByRequestedTime:
+                  severeImpact.hospitalBedsByRequestedTime,
+              },
+              {
+                casesForICUByRequestedTime:
+                  severeImpact.casesForICUByRequestedTime,
+              },
+              {
+                casesForVentilatorsByRequestedTime:
+                  severeImpact.casesForVentilatorsByRequestedTime,
+              },
+              { dollarsInFlight: severeImpact.dollarsInFlight },
+            ],
+          },
+        ],
+      },
+    ];
+
+    //set the content type to xml
+    res.type('application/xml');
+    //send an xml data
+    res.status(201).send(xml(dataToSubmit, { declaration: true }));
   }
 });
 
 app.get('/api/v1/on-covid-19/logs', (req, res) => {
-  fs.readFile('./src/logs.json', (err, jsonString) => {
-    if (err) {
-      console.log('failed to read', err);
-    } else {
-      try {
-        const log = JSON.parse(jsonString);
-
-        res.format({
-          'text/plain': () => res.send(log),
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    }
+  const logs = fs.readFileSync(path.join(__dirname, './logs/log.txt'), {
+    encoding: 'utf-8',
   });
+  console.log(logs);
+  res.type('text/plain');
+  res.status(200).send(logs);
 });
 
-app.listen(PORT, () => debug.log(`LISTENING ON PORT ${PORT}`));
+app.get('*', (req, res) => {
+  res.status(403).send('forbidden');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
